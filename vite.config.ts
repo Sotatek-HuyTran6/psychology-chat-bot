5511b3d052de35e47781016ab0f2b11ec60362da
sotatek <kwon26@inzent.com>
2025-12-01 23:52:50 +0700

    update: done

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/psychology-chat-bot/', // tên repo trên GitHub
+  base: '/', // tên repo trên GitHub
   plugins: [react(), tailwindcss()],
 });
